937e95caff3744af0839123c94d926c54d88d245
deanbrophy <dean.brophy@gmail.com>
2015-04-07 01:09:24 -0400

    Solr is working!!!!!
    Solr is working and hitting my database!!!

---
+++ SolrSQLPOC/PreciseSolrVagrant/fancycopy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo cp  /media/psf/vagrant/solrinstallfiles/sqljdbc4.jar /home/vagrant/solr-4.7.2/contrib/dataimporthandler/lib/
+sudo cp -r /media/psf/vagrant/solrinstallfiles/FancySearchCollection  /home/vagrant/solr-4.7.2/example/solr/FancySearchCollection
